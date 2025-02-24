63eeb793f6169f364bbfb6a4e500138f709eda1e
Marcos Henrique <sena_marcos.fp@hotmail.com>
2025-02-24 11:23:21 -0300

    Fotos e pagina de comentarios finalizadas

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,10 @@ const App = () => {
     <div>
       <Header />
       <Main /> 
-      <Contact />
       <Photos />
-      <Coments />
+      <Contact />
       <Footer />
+      <Coments />
     </div>
   );
 };
--- src/Components/Coments.jsx
+++ src/Components/Coments.jsx
@@ -1,7 +1,100 @@
 import React from "react";
+import "../assets/Coments.css";
+import profile_1 from "../assets/profile-1.jpg"
+import profile_2 from "../assets/profile-2.jpg"
+import profile_3 from "../assets/profile-3.jpg"
 
 const Coments = () => {
-  return <div>Coments</div>;
+  return (
+    <>
+      <div className="testimonials">
+        <div className="testimonial">
+          <p>
+            Fylo has improved our team productivity by an order of magnitude.
+            Since making the switch our team has become a well-oiled
+            collaboration machine.
+          </p>
+          <div className="profile">
+            <img src={profile_1} />
+            <div className="profile-description">
+              <h3>Satish Patel</h3>
+              <p>commentator</p>
+            </div>
+          </div>
+        </div>
+        <div className="testimonial">
+          <p>
+            Fylo has improved our team productivity by an order of magnitude.
+            Since making the switch our team has become a well-oiled
+            collaboration machine.
+          </p>
+          <div className="profile">
+            <img src={profile_2} />
+            <div className="profile-description">
+              <h3>Bruce McKenzie</h3>
+              <p>commentator</p>
+            </div>
+          </div>
+        </div>
+        <div className="testimonial">
+          <p>
+            Fylo has improved our team productivity by an order of magnitude.
+            Since making the switch our team has become a well-oiled
+            collaboration machine.
+          </p>
+          <div className="profile">
+            <img src={profile_3} />
+            <div className="profile-description">
+              <h3>Iva Boyd</h3>
+              <p>commentator</p>
+            </div>
+          </div>
+        </div>
+        <div className="testimonial">
+          <p>
+            Fylo has improved our team productivity by an order of magnitude.
+            Since making the switch our team has become a well-oiled
+            collaboration machine.
+          </p>
+          <div className="profile">
+            <img src={profile_1} />
+            <div className="profile-description">
+              <h3>Satish Patel</h3>
+              <p>commentator</p>
+            </div>
+          </div>
+        </div>
+        <div className="testimonial">
+          <p>
+            Fylo has improved our team productivity by an order of magnitude.
+            Since making the switch our team has become a well-oiled
+            collaboration machine.
+          </p>
+          <div className="profile">
+            <img src={profile_2} />
+            <div className="profile-description">
+              <h3>Bruce McKenzie</h3>
+              <p>commentator</p>
+            </div>
+          </div>
+        </div>
+        <div className="testimonial">
+          <p>
+            Fylo has improved our team productivity by an order of magnitude.
+            Since making the switch our team has become a well-oiled
+            collaboration machine.
+          </p>
+          <div className="profile">
+            <img src={profile_3} />
+            <div className="profile-description">
+              <h3>Iva Boyd</h3>
+              <p>commentator</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Coments;
